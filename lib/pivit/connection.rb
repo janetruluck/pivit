6b9d02bc45d774f3501651e6c5d12867764104e2
Jason Truluck <jason.truluck@gmail.com>
2013-04-25 16:28:56 -0400

    add connection

---
+++ lib/pivit/connection.rb
@@ -0,0 +1,22 @@
+# Connection
+require "faraday_middleware"
+
+module Pivit
+  module Connection
+    def connection(options = {})
+      options = {
+        :ssl              => { :verify => false }
+      }.merge(options)
+  
+      connection = Faraday.new(options) do |build|
+        build.use FaradayMiddleware::Mashify
+        build.use FaradayMiddleware::ParseXml,  :content_type => /\bxml$/
+        build.adapter  Faraday.default_adapter
+      end
+
+      connection.headers["X-TrackerToken"] = api_token if self.authenticated?
+
+      connection
+    end
+  end
+end
