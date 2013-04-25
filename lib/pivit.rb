9b17ac32eab3a2b618ca258ef6e278f43200145c
Jason Truluck <jason.truluck@gmail.com>
2013-04-25 16:29:29 -0400

    add base class

--- lib/pivit.rb
+++ lib/pivit.rb
@@ -1,5 +1,20 @@
 require "pivit/version"
+require "pivit/configuration"
+require "pivit/client"
+require "pivit/error"
 
 module Pivit
-  # Your code goes here...
+  extend Configuration
+
+  class << self
+    # Alias for Pivit::Client.new
+    # @return [Pivit::Client]
+    def new(options = {})
+      Pivit::Client.new(options)
+    end
+
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
+  end
 end
