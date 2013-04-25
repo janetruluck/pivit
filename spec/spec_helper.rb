69babe32427439914f69c008a09010c653d4aec6
Jason Truluck <jason.truluck@gmail.com>
2013-04-25 16:28:00 -0400

    add colorize to spechelper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails'
+  class SimpleCov::Formatter::QualityFormatter
+    def format(result)
+      SimpleCov::Formatter::HTMLFormatter.new.format(result)
+      File.open("coverage/covered_percent", "w") do |f|
+        f.puts result.source_files.covered_percent.to_f
+      end
+    end
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::QualityFormatter
+end
+
+require 'pivit'
+require 'vcr'
+require "mocha/api"
+
+authentications = File.expand_path("../fixtures/authentications.yml", __FILE__)
+if File.exists?(authentications)
+  ENV.update YAML::load(File.open(authentications))
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :faraday
+  # Uncomment if you need to log VCR
+  # c.debug_logger = File.open(Rails.root.join("log","vcr_debugger.log"), 'w')
+  c.configure_rspec_metadata!
+end
+
+Dir[File.expand_path("spec/support/**/*.rb", __FILE__)].each {|f| require f}
+
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.order                                           = "random"
+  config.color_enabled = true
+end
