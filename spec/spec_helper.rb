c671885a9865dab2c512abcc1c874c106ae26bb6
Jason Truluck <jason.truluck@gmail.com>
2013-04-28 18:52:16 -0400

    integrate webmock to allow for static http requests not recorded by vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,7 @@ end
 
 require 'pivit'
 require 'vcr'
+require 'webmock/rspec'
 require "mocha/api"
 
 authentications = File.expand_path("../fixtures/authentications.yml", __FILE__)
@@ -29,10 +30,10 @@ end
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :faraday
-  # Uncomment if you need to log VCR
-  # c.debug_logger = File.open(Rails.root.join("log","vcr_debugger.log"), 'w')
   c.configure_rspec_metadata!
 end
+
+WebMock.allow_net_connect!
 
 Dir[File.expand_path("spec/support/**/*.rb", __FILE__)].each {|f| require f}
 
@@ -40,4 +41,14 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order                                           = "random"
   config.color_enabled = true
+
+  config.before :type => :webmock do
+    WebMock.disable_net_connect!
+    VCR.turn_off!
+  end
+
+  config.after :type => :webmock do
+    WebMock.allow_net_connect!
+    VCR.turn_on!
+  end
 end
