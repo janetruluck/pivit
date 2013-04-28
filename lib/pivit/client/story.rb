db46a0dd6bfe02c689e7dc2f25aa4936c032ee61
Jason Truluck <jason.truluck@gmail.com>
2013-04-28 18:50:22 -0400

    add story methods and specs

---
+++ lib/pivit/client/story.rb
@@ -0,0 +1,159 @@
+# Stories
+
+module Pivit
+  class Client
+    # Story management
+    # 
+    # @see http://www.pivotaltracker.com/help/api?version=v3#getting_stories
+    module Story
+      # Retrieve a single story from your account
+      #
+      # @see http://www.pivotaltracker.com/help/api?version=v3#getting_stories
+      #
+      # @param [Integer] project_id the id of the project that you want to
+      # retrieve stories from
+      # @param [Integer] story_id the id of the story that you want to
+      # retrieve
+      #
+      # @param [Integer] project_id the id of the project that contains the
+      # story
+      #
+      # @return [Hashie::Mash] story response
+      #
+      # @example 
+      #   Pivit::Client.story(1111111, 123456)
+      #
+      # @author Jason Truluck
+      def story(project_id, story_id, options = {})
+        get("projects/#{project_id}/stories/#{story_id}", options).story
+      end
+
+      # Retrieve all stories from your account
+      #
+      # @see http://www.pivotaltracker.com/help/api?version=v3#getting_stories
+      #
+      # @param [Integer] project_id the id of the project that contains the
+      #stories
+      #
+      # @return [Hashie::Mash] stories response
+      #
+      # @example 
+      #   Pivit::Client.stories(1111111)
+      # 
+      # @author Jason Truluck
+      def stories(project_id, options = {})
+        get("projects/#{project_id}/stories/", options).stories
+      end
+
+      # Create a story
+      # 
+      # Provide the parameters you want to use for the story via the options
+      # hash 
+      #
+      # @see http://www.pivotaltracker.com/help/api?version=v3#add_story
+      #
+      # @param [Integer] project_id the id of the project that contains the
+      # story
+      #
+      # @return [Hashie::Mash] story created response
+      #
+      # @example 
+      #   Pivit::Client.create_story_type=> "feature", :name => "Story"})
+      #
+      # @author Jason Truluck
+      def create_story(project_id, options = {})
+        options = { :story => options }
+        post("projects/#{project_id}/stories", options).story
+      end
+
+      # Update a story
+      # 
+      # Provide the parameters you want to use for the story via the options
+      # hash 
+      #
+      # @see http://www.pivotaltracker.com/help/api?version=v3#update_story
+      #
+      # @param [Integer] project_id the id of the project that contains the
+      # story
+      # @param [Integer] story_id the id of the story that is getting updated
+      #
+      # @return [Hashie::Mash] story updated response
+      #
+      # @example 
+      #   Pivit::Client.update_story(12345, 11111, { :name => "awesome new story name"})
+      #
+      # @author Jason Truluck
+      def update_story(project_id, story_id, options = {})
+        options = { :story => options }
+        put("projects/#{project_id}/stories/#{story_id}",  options).story
+      end
+    
+      # Delete a story
+      # 
+      #
+      # @see http://www.pivotaltracker.com/help/api?version=v3#delete_story
+      #
+      # @param [Integer] project_id the id of the project that contains the
+      # story
+      # @param [Integer] story_id the id of the story that is getting deleted
+      #
+      # @return [Hashie::Mash] story deleted response
+      #
+      # @example 
+      #   Pivit::Client.update_story(12345, 11111)
+      ##
+      # @author Jason Truluck
+      def delete_story(project_id, story_id, options = {})
+        delete("projects/#{project_id}/stories/#{story_id}", options).story
+      end
+     
+      # Move a story before another story
+      #
+      # @see http://www.pivotaltracker.com/help/api?version=v3#move_story
+      #
+      # @param [Integer] project_id the id of the project that contains the
+      # story
+      # @param [Integer] story_id the id of the story that is getting moved
+      # @param [Integer] story_target_id the id of the story that the other
+      # story is moving before
+      #
+      # @return [Hashie::Mash] story move response
+      #
+      # @example 
+      #   Pivit::Client.move_story_before(12345, 11111, 22222)
+      ##
+      # @author Jason Truluck
+      def move_story_before(project_id, story_id, story_target_id, options = {})
+        move_story(project_id, story_id, story_target_id, :before, options)
+      end
+      
+      # Move a story after another story
+      #
+      # @see http://www.pivotaltracker.com/help/api?version=v3#move_story
+      #
+      # @param [Integer] project_id the id of the project that contains the
+      # story
+      # @param [Integer] story_id the id of the story that is getting moved
+      # @param [Integer] story_target_id the id of the story that the other
+      # story is moving after
+      #
+      # @return [Hashie::Mash] story move response
+      #
+      # @example 
+      #   Pivit::Client.move_story_after(12345, 11111, 22222)
+      ##
+      # @author Jason Truluck
+      def move_story_after(project_id, story_id, story_target_id, options = {})
+        move_story(project_id, story_id, story_target_id, :after, options)
+      end
+
+
+
+      private
+      def move_story(project_id, story_id, story_target_id, direction, options = {})
+        options.merge!("move\[move\]" => direction, "move\[target\]" =>  story_target_id)
+        post("projects/#{project_id}/stories/#{story_id}/moves", options).story
+      end
+    end
+  end
+end
---
+++ spec/pivit/client/story_spec.rb
@@ -0,0 +1,143 @@
+require "spec_helper"
+
+# Story Spec
+describe Pivit::Client::Story do
+  before do
+    Pivit.reset!
+  end
+
+  let!(:pivit) { Pivit::Client.new(:token=> ENV["TOKEN"]) }
+
+  describe ".story", :vcr do
+    let(:current_response) { pivit.story(ENV["PROJECT"], ENV["STORY"]) }
+
+    it "should return the story response" do
+      current_response.should_not be_nil
+      current_response.should respond_to(:name)
+    end
+  end
+
+  describe ".stories", :vcr => {:cassette_name => "story/stories"} do
+    let(:current_response) { pivit.stories(ENV["PROJECT"]) }
+
+    it "should return an array of stories" do
+      current_response.should be_a(Array)
+      current_response.first.should respond_to(:current_state)
+    end
+
+    it "should return the stories" do
+      current_response.should_not be_nil
+    end
+  end
+
+  describe ".create_story", :vcr => {:cassette_name => "story/create_story"} do
+    let(:current_response) { 
+      pivit.create_story(ENV["PROJECT"], 
+                      {
+                        :name => "Awesome Test story",
+                        :story_type =>"feature"
+                      }) 
+    }
+
+    it "returns the story that was created" do
+      current_response.should respond_to(:name)
+    end
+
+    it "should be a hashie" do
+      current_response.should be_a(Hashie::Mash)
+    end
+  end
+
+  describe ".update_story", :vcr => {:cassette_name => "story/update_story"} do
+    let(:name) { "awesome new name" }
+    let(:current_response) {  
+      pivit.update_story(ENV["PROJECT"], 
+                         ENV["STORY"],
+                        {
+                          :name => name
+                        }) 
+    }
+
+    it "returns the story that was update" do
+      current_response.should respond_to(:name)
+    end
+
+    it "updates the attributes specified" do
+      current_response.name.should == name
+    end
+
+    it "should be a hashie" do
+      current_response.should be_a(Hashie::Mash)
+    end
+  end
+
+  describe ".delete_story", :type => :webmock do
+    let(:current_response) { pivit.delete_story(ENV["PROJECT"], ENV["STORY"]) }
+
+    it "returns the story that was deleted" do
+      stub_request(:delete, "https://www.pivotaltracker.com/services/v3/projects/795721/stories/48859617").
+        to_return(:status => 200,
+                  :body => File.open(File.expand_path("../../../fixtures/stubs/story/delete_story.xml", __FILE__)),
+                  :headers => {'Accept' => 'application/xml', 'Content-type' => 'application/xml',})
+
+      current_response.id.should == ENV["STORY"].to_i
+    end
+
+    it "should be a hashie" do
+      stub_request(:delete, "https://www.pivotaltracker.com/services/v3/projects/795721/stories/48859617").
+        to_return(:status => 200,
+                  :body => File.open(File.expand_path("../../../fixtures/stubs/story/delete_story.xml", __FILE__)),
+                  :headers => {'Accept' => 'application/xml', 'Content-type' => 'application/xml',})
+
+      current_response.should be_a(Hashie::Mash)
+    end
+  end
+
+  describe ".move_story_before", :type => :webmock do
+    let(:current_response) { pivit.move_story_before(ENV["PROJECT"], ENV["STORY"], ENV["STORY_B"]) }
+
+    it "returns the story that was moved" do
+      stub_request(:post, "https://www.pivotaltracker.com/services/v3/projects/795721/stories/48859617/moves?move%5Bmove%5D=before&move%5Btarget%5D=47998079").
+        with(:headers => {'Accept'=>'*/*', 'Content-Length'=>'0', 'User-Agent'=>'Faraday v0.8.7', 'X-Trackertoken'=>'a8edd0e3de062a74f94ce7284e85f618'}).
+        to_return(:status => 200, 
+                  :body => File.open(File.expand_path("../../../fixtures/stubs/story/delete_story.xml", __FILE__)),
+                  :headers => {'Accept' => 'application/xml', 'Content-type' => 'application/xml',})
+
+      current_response.id.should == ENV["STORY"].to_i
+    end
+
+    it "should be a hashie" do
+      stub_request(:post, "https://www.pivotaltracker.com/services/v3/projects/795721/stories/48859617/moves?move%5Bmove%5D=before&move%5Btarget%5D=47998079").
+        with(:headers => {'Accept'=>'*/*', 'Content-Length'=>'0', 'User-Agent'=>'Faraday v0.8.7', 'X-Trackertoken'=>'a8edd0e3de062a74f94ce7284e85f618'}).
+        to_return(:status => 200, 
+                  :body => File.open(File.expand_path("../../../fixtures/stubs/story/delete_story.xml", __FILE__)),
+                  :headers => {'Accept' => 'application/xml', 'Content-type' => 'application/xml',})
+
+      current_response.should be_a(Hashie::Mash)
+    end
+  end
+
+  describe ".move_story_after", :type => :webmock do
+    let(:current_response) { pivit.move_story_after(ENV["PROJECT"], ENV["STORY"], ENV["STORY_B"]) }
+
+    it "returns the story that was moved" do
+      stub_request(:post, "https://www.pivotaltracker.com/services/v3/projects/795721/stories/48859617/moves?move%5Bmove%5D=after&move%5Btarget%5D=47998079").
+        with(:headers => {'Accept'=>'*/*', 'Content-Length'=>'0', 'User-Agent'=>'Faraday v0.8.7', 'X-Trackertoken'=>'a8edd0e3de062a74f94ce7284e85f618'}).
+        to_return(:status => 200, 
+                  :body => File.open(File.expand_path("../../../fixtures/stubs/story/delete_story.xml", __FILE__)),
+                  :headers => {'Accept' => 'application/xml', 'Content-type' => 'application/xml',})
+
+      current_response.id.should == ENV["STORY"].to_i
+    end
+
+    it "should be a hashie" do
+      stub_request(:post, "https://www.pivotaltracker.com/services/v3/projects/795721/stories/48859617/moves?move%5Bmove%5D=after&move%5Btarget%5D=47998079").
+        with(:headers => {'Accept'=>'*/*', 'Content-Length'=>'0', 'User-Agent'=>'Faraday v0.8.7', 'X-Trackertoken'=>'a8edd0e3de062a74f94ce7284e85f618'}).
+        to_return(:status => 200, 
+                  :body => File.open(File.expand_path("../../../fixtures/stubs/story/delete_story.xml", __FILE__)),
+                  :headers => {'Accept' => 'application/xml', 'Content-type' => 'application/xml',})
+
+      current_response.should be_a(Hashie::Mash)
+    end
+  end
+end
