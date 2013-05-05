4f2ef72e6ae9e0754bf5df206d48fe8463774729
Jason Truluck <jason.truluck@gmail.com>
2013-05-05 18:25:33 -0400

    fix project documentation

--- lib/pivit/client/project.rb
+++ lib/pivit/client/project.rb
@@ -1,15 +1,14 @@
 module Pivit
   class Client
     # Project management
-    # 
+    #
     # @see http://www.pivotaltracker.com/help/api?version=v3#getting_projects
     module Project
       # Retrieve a single project from your account
       #
-      # @see http://www.pivotaltracker.com/help/api?version=v3#getting_projects
+      # @see https://www.pivotaltracker.com/help/api?version=v3#get_project_info
       #
-      # @param [Integer] project_id the id of the project that you want to
-      # retrieve
+      # @param [Integer] project_id the id of the project that you want to retrieve
       #
       # @return [Hashie::Mash] project response
       #
@@ -23,31 +22,29 @@ module Pivit
 
       # Retrieve all projects from your account
       #
-      # @see http://www.pivotaltracker.com/help/api?version=v3#getting_projects
+      # @see https://www.pivotaltracker.com/help/api?version=v3#get_project_all_projects
       #
       # @return [Hashie::Mash] projects response
       #
-      # @example 
-      #   Pivit::Client.project      
-      # 
+      # @example
+      #   Pivit::Client.projects
+      #
       # @author Jason Truluck
       def projects(options = {})
         get("projects", options).projects
       end
 
       # Create a Project
-      # 
-      # Provide the parameters you wantt to use for the project via the options
-      # hash 
+      #
+      # Provide the parameters you wantt to use for the project via the options hash
       #
       # @see http://www.pivotaltracker.com/help/api?version=v3#create_project
       #
       # @return [Hashie::Mash] project created response
       #
-      # @example 
-      #   Pivit::Client.create_project({:name => "Test Project", :iteration_length
-      #   => "2"})
-      # 
+      # @example
+      #   Pivit::Client.create_project({:name => "Test Project", :iteration_length => "2"})
+      #
       # @author Jason Truluck
       def create_project(options = {})
         options = { :project => options }
